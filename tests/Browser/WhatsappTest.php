d6ca6c0bfd737b501e898ff2522487d4440e9122
rcwjys <rcwjys@gmail.com>
2024-06-10 09:40:52 +0700

    finalizinng test for whatsapp

--- tests/Browser/WhatsappTest.php
+++ tests/Browser/WhatsappTest.php
@@ -13,13 +13,30 @@ class WhatsappTest extends DuskTestCase
      */
     public function testExample(): void
     {
+        // $this->browse(function (Browser $browser) {
+        //     $browser->visit('/')
+        //         ->clickLink('Hubungi Kami')
+        //         ->pause(1000)
+        //         ->switchToWindow(function ($driver) {
+        //             return strpos($driver->getCurrentURL(), 'https://api.whatsapp.com/send?phone=6282158204550') !== false;
+        //         });
+        // });
+
+        // $this->browse(function (Browser $browser) {
+        //     $browser->visit('/')
+        //         ->script('window.open("https://api.whatsapp.com/send?phone=6282158204550");');
+        //     ->pause(1000) 
+        //     ->switchToWindow(collect($browser->driver->getWindowHandles())->last()) 
+        //     ->assertUrlIs('https://api.whatsapp.com/send?phone=6282158204550');
+        // });
+
+
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                 ->clickLink('Hubungi Kami')
-                ->pause(1000)
-                ->switchToWindow(function ($driver) {
-                    return strpos($driver->getCurrentURL(), 'https://api.whatsapp.com/send?phone=6282158204550') !== false;
-                });
+                ->pause(1000) // Pause to allow time for the new tab to open
+                ->switchToWindow(collect($browser->driver->getWindowHandles())->last())
+                ->assertUrlIs('https://api.whatsapp.com/send?phone=6282158204550');
         });
     }
 }
